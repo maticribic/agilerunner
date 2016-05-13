21b2684d2cfbdf00257f52c695a0b57ddef075d2
Žana Hrastovšek <zana.hrastovsek@solvera-lynx.com>
2016-05-13 13:55:21 +0200

    Config.

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git update-index --assume-unchanged .idea/encodings.xml
+git update-index --assume-unchanged .idea/workspace.xml
+git update-index --assume-unchanged .idea/gradle.xml
+git update-index --assume-unchanged .idea/misc.xml
+git update-index --assume-unchanged .idea/vcs.xml
+
